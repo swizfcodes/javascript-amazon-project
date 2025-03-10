9dcd64816750460489ba8e65cf42e37710cd7bda
Swizfcodes <elishaokoh897@gmail.com>
2025-03-10 16:27:49 +0100

    Fix price formatting and Radio button selection

--- script/amazon.js
+++ script/amazon.js
@@ -1,5 +1,6 @@
 import { cart, addProductToCart } from '../data/cart.js';
 import { products } from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 let productsHTML = '';
 
@@ -24,7 +25,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-price">
-       $${(product.priceCents / 100).toFixed(2)}
+       $${formatCurrency(product.priceCents)}
       </div>
 
       <div class="product-quantity-container">
@@ -73,6 +74,6 @@ document.querySelectorAll('.js-add-to-cart-button').forEach((button) => {
   button.addEventListener('click', () => { 
     const productId = button.dataset.productId;
     addProductToCart(productId);
-    updateCartQuantity();
+    updateCartQuantity();;
   });
 });
