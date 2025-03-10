a8f0cd5bac289971057f37fe7d510163ec6061d2
Swizfcodes <elishaokoh897@gmail.com>
2025-03-10 16:46:40 +0100

    Delete products from cart when the user clicks on the delete button

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [{
+export let cart = [{
   productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity : 2,
 },
@@ -27,4 +27,15 @@ export function addProductToCart(productId) {
       quantity: 1
     });
   }
+}
+
+export function removeProductFromCart(productId) {
+  const newCart = [];
+  cart.forEach((cartItem) => {
+    if (productId !== cartItem.productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
 }
