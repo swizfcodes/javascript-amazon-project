34dadf357fea21ad17e81785d80ecab45c1137a0
Swizfcodes <elishaokoh897@gmail.com>
2025-03-19 15:43:22 +0100

    Make Payment summary interactive

--- script/checkout/orderSummary.js
+++ script/checkout/orderSummary.js
@@ -3,11 +3,7 @@ import { products, getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
-
-
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-deliveryDate.format('dddd, MMMM D');
+import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary(){
   const today = dayjs();
@@ -119,6 +115,7 @@ export function renderOrderSummary(){
       removeProductFromCart(productId);
       
       document.querySelector(`.js-cart-item-container-${productId}`).remove();
+      renderPaymentSummary();
 
     });
   });
@@ -128,6 +125,7 @@ export function renderOrderSummary(){
       const { productId, deliveryOptionId } = element.dataset;
       updateDeliveryOptions(productId, deliveryOptionId);
       renderOrderSummary();
+      renderPaymentSummary();
     });
   });
 }
