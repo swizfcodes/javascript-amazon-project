1363f628168ab7c4290b114617962ddb41d98b9a
Swizfcodes <elishaokoh897@gmail.com>
2025-04-09 15:45:16 +0100

    Learnt HTML and CSS then designed YouTube Homepage

--- test/moneyTest.js
+++ test/moneyTest.js
@@ -3,5 +3,5 @@ import { formatCurrency } from "../script/utils/money.js";
 if (formatCurrency(2095) === '20.95') {
   console.log('passed');
 }else {
-  console.log('failed');
+  console.log('failed'); 
 }
